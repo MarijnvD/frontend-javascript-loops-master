b9e2fb5574fc6ead3ac946c137a58ec7fc31e4d3
MarijnvD <marijnvandorp@gmail.com>
2022-03-27 00:47:30 +0100

    uitgewerkte opdracht

--- loops.js
+++ loops.js
@@ -6,7 +6,9 @@
 // Hoera!
 // ==========================================
 
-
+for (let i = 0; i < 3; i++) {
+    console.log("Hoera!");
+}
 
 // ==========================================
 // Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
@@ -18,22 +20,34 @@
 // klaar!
 // ==========================================
 
-
+for (let i = 0; i < 5; i++) {
+    if (i > 3){
+        console.log("Klaar!");
+    }else{
+        console.log("loop....");
+    }
+}
 
 // ==========================================
 // Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
 // Verwachte uitkomsten:
 // 0 is even
-// 1 is oneven
+// 1 is oneven4
 // 2 is even
 // 3 is oneven
 // .... etc.
 // 15 is oneven
 
-// Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
+// Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over..
 // ==========================================
 
-
+for (let i = 0; i < 16; i++) {
+    if (i % 2 === 0){
+        console.log(i +" is even");
+    }else{
+        console.log(i +" is oneven");
+    }
+}
 
 // ==========================================
 // [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
@@ -51,3 +65,12 @@
 // >> 9
 // ==========================================
 
+for (let i = 0; i < 10; i++) {
+    if (i > 5){
+        console.log(">> " + i);
+    }else if (i > 2) {
+        console.log("> " + i);
+    }else{
+        console.log(i);
+    }
+}
